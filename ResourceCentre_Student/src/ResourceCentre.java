3449abb2235ff6cba256762cfa528617c27ba715
turtl <turtl@LAPTOP-935PS8U7.mshome.net>
2023-07-10 14:21:42 +0800

    update viewAllChromebood() in ResourCentre

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -236,7 +236,6 @@ public class ResourceCentre {
 		}
 		return isLoaned;
 	}
-	
 	public static void loanCamcorder(ArrayList<Camcorder> camcorderList) {
 		ResourceCentre.viewAllCamcorder(camcorderList);
 		String tag = Helper.readString("Enter asset tag > ");
